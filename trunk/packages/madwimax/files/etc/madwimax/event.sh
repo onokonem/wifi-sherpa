86c0adbf7f1afcd02eea951f79f93062811b5076
onokonem <onokonem@33e8e072-88cd-11de-8988-25e8a898b690>
2009-08-27 10:04:19 +0000

    fix: fixMTU is numerical, firewall restarted on each wimax up to catch a new wan iface name

--- trunk/packages/madwimax/files/etc/madwimax/event.sh
+++ trunk/packages/madwimax/files/etc/madwimax/event.sh
@@ -4,9 +4,10 @@ case "$1" in
         if-create)
                 ;;
         if-up)
-                ifName="`ifconfig|grep wimax|awk '{print $1}'`"
+                ifName="`ifconfig|grep wimax|head -n 1|awk '{print $1}'`"
                 uci set network.wan.ifname="$ifName"
                 /sbin/ifup wan
+                /etc/init.d/firewall restart
                 ;;
         if-down)
                 /sbin/ifdown wan
