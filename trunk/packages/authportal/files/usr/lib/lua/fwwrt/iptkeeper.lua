8c0f4e0d480f0318cd64ce704c768ce70459b2ac
onokonem <onokonem@33e8e072-88cd-11de-8988-25e8a898b690>
2009-08-31 16:54:12 +0000

    fix: ip-specific rules are changed only then necessay (no total remove on each update), DB is a single active users info source now, some code cleanup

--- trunk/packages/authportal/files/usr/lib/lua/fwwrt/iptkeeper.lua
+++ trunk/packages/authportal/files/usr/lib/lua/fwwrt/iptkeeper.lua
@@ -11,50 +11,28 @@ module("fwwrt.iptkeeper", package.seeall)
 
 require "fwwrt.util"
 require "fwwrt.authportal"
+require "fwwrt.iptables"
 
 -------------------------------------------------------------
 -------------------------------------------------------------
 -------------------------------------------------------------
 
 local leasesFileName  = fwwrt.util.uciGet('dhcp.dnsmasq.leasefile', 'string')
-local iptablesPath    = fwwrt.util.uciGet('fwwrt.firewall.iptables', 'string')
 
-local loggedInTableChanged = false
-local loggedIn             = {}
 local leases               = {}
 
 function getMac(ip)
-	if (leases[ip]) then return leases[ip].mac end
-	end
-
-function logIpIn(ip, expire)
-	if (leases[ip] == nil)
-		then
-		fwwrt.util.logger("LOG_ERR", "Address '"..tostring(ip).."' rejected: no lease found")
-		return false
-		end
-	fwwrt.util.logger("LOG_NOTICE", "'"..tostring(ip).."' logged in with MAC '"..tostring(leases[ip].mac).."' till "..os.date("%Y%m%d-%H:%M:%S", expire))
-	loggedIn[ip] = {["mac"] = leases[ip].mac, ["expire"] = expire + 0}
-	loggedInTableChanged = true
-	return true
-	end
-
-function logIpOut(ip)
-	loggedIn[ip] = nil
-	loggedInTableChanged = true
-	fwwrt.authportal.doLogout(ip)
-	fwwrt.util.logger("LOG_INFO", "'"..tostring(ip).."' logged out by request")
-	return true
+	return leases[ip] and leases[ip].mac
 	end
 
 function readLeases (leasesFileName, curTime)
 	fwwrt.util.logger("LOG_DEBUG", "Updating dhcp info from '"..tostring(leasesFileName).."'")
 	local leases = {}
-	local file, error = io.open(leasesFileName, "r")
-	if (file ~= nil)
+	local reader, error = io.open(leasesFileName, "r")
+	if (reader ~= nil)
 		then
 		local line = nil
-		for line in file:lines()
+		for line in reader:lines()
 			do
 			local _, _, expire, mac, ip = string.find(line, "^(%d+)%s+(%S+)%s+(%S+)%s+%S+%s+%S+%s*$")
 			expire=expire + 0
@@ -65,7 +43,7 @@ function readLeases (leasesFileName, curTime)
 				end
 			end
 
-		file:close()
+		reader:close()
 	else
 		fwwrt.util.logger("LOG_ALERT", "Can not open file '"..tostring(leasesFileName).."' for read: "..tostring(error))
 		end
@@ -73,61 +51,21 @@ function readLeases (leasesFileName, curTime)
 	end
 
 function loggedInCleanup (loggedIn, leases, curTime)
-	local tmpTable = {}
+    local loggedIn = fwwrt.authportal.getActiveUsers()
 	local ip, info
 	for ip, info in pairs(loggedIn)
 		do
 		if (not (leases[ip] and leases[ip].expire > curTime))
 			then
-			fwwrt.authportal.doLogout(ip)
-			loggedInTableChanged = true
-			fwwrt.util.logger("LOG_INFO", "'"..tostring(ip).."' logged out as not renewed")
+			fwwrt.authportal.doLogout(ip, "not renewed")
 		elseif (info.mac ~= leases[ip].mac)
 			then
-			fwwrt.authportal.doLogout(ip)
-			loggedInTableChanged = true
-			fwwrt.util.logger("LOG_INFO", "'"..tostring(ip).."' logged out as MAC changed")
+			fwwrt.authportal.doLogout(ip, "MAC changed")
 		elseif (info.expire < curTime)
 			then
-			fwwrt.authportal.doLogout(ip)
-			loggedInTableChanged = true
-			fwwrt.util.logger("LOG_INFO", "'"..tostring(ip).."' logged out as expired")
-		else
-			tmpTable[ip] = info
+			fwwrt.authportal.doLogout(ip, "expired")
 			end
 		end
-	return tmpTable
-	end
-
-function os_execute(command)
-    local res = os.execute(command)
-	fwwrt.util.logger(((res == 0) and "LOG_DEBUG" or "LOG_ALERT"), "Command '"..tostring(command).."' completed with code "..tostring(res))
-	end
-
-function iptCleanup()
-	fwwrt.util.logger("LOG_DEBUG", "Cleaning access list")
-	os_execute(iptablesPath.." -t nat --flush checkauth_PREROUTING")
-	os_execute(iptablesPath.." -t nat --flush checkauth_POSTROUTING")
-	os_execute(iptablesPath.."        --flush checkauth_FORWARD")
-	end
-
-iptCleanup()
-
-local function iptAddRule(iptablesPath, tableName, chainName, conditions, destination)
-    local rulePrefix = iptablesPath.." "..tableName.." -A "..chainName.."  "..conditions.." -j "
-    if (fwwrt.util.loggerVerbose())
-    	then
-    	os_execute(rulePrefix.."LOG --log-level debug --log-prefix '"..chainName.."'")
-    	end
-	os_execute(rulePrefix..destination)
-	end
-
-
-function iptAllowIP(ip)
-	fwwrt.util.logger("LOG_DEBUG", "Adding '"..tostring(ip).."' to access list")
-	iptAddRule(iptablesPath, '-t nat', 'checkauth_PREROUTING',  '-s '..ip, 'authorized_PREROUTING')
-	iptAddRule(iptablesPath, '-t nat', 'checkauth_POSTROUTING', '-s '..ip, 'authorized_POSTROUTING')
-	iptAddRule(iptablesPath, '',       'checkauth_FORWARD',     '-s '..ip, 'authorized_FORWARD')
 	end
 
 
@@ -135,16 +73,22 @@ function updateAccess()
 	local curTime = os.time()
 
 	leases   = readLeases(leasesFileName, curTime)
-	loggedIn = loggedInCleanup(loggedIn, leases, curTime)
 
-	if (loggedInTableChanged)
-		then
-		fwwrt.util.logger("LOG_NOTICE", "Updating access list")
-		iptCleanup()
-		for ip, info in pairs(loggedIn)
-			do iptAllowIP(ip) end
+	local ip, info
+	for ip, info in pairs(fwwrt.authportal.getActiveUsers())
+		do
+		if (not (leases[ip] and leases[ip].expire > curTime))
+			then
+			fwwrt.authportal.doLogout(ip, "not renewed")
+		elseif (info.mac ~= leases[ip].mac)
+			then
+			fwwrt.authportal.doLogout(ip, "MAC changed")
+		elseif (info.expire < curTime)
+			then
+			fwwrt.authportal.doLogout(ip, "expired")
+			end
 		end
 
-	loggedInTableChanged = false
+	fwwrt.iptables.syncChains(fwwrt.authportal.getActiveUsers())
 	end
 
